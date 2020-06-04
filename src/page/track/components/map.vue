541153caede4b20e7f3281388e65e44acc28848c
邓鑫波 <mymail33333@163.com>
2020-06-04 12:49:55 +0800

    轨迹地图

--- src/page/track/components/map.vue
+++ src/page/track/components/map.vue
@@ -17,63 +17,111 @@
         methods: {
             initMap() {
                 const series = this.initSeries();
+                const {options, timelineData} = this.initOptions()
                 var option = {
-                    tooltip: {
-                        trigger: 'item',
-                        backgroundColor: 'rgba(166, 200, 76, 0.82)',
-                        borderColor: '#FFFFCC',
-                        showDelay: 0,
-                        hideDelay: 0,
-                        enterable: true,
-                        transitionDuration: 0,
-                        extraCssText: 'z-index:100',
-                        // formatter: function(params, ticket, callback) {
-                        //     //根据业务自己拓展要显示的内容
-                        //     var res = "";
-                        //     var name = params.name;
-                        //     var value = params.value[params.seriesIndex + 1];
-                        //     res = "<span style='color:#fff;'>" + name + "</span><br/>数据：" + value;
-                        //     return res;
-                        // }
-                    },
-                    backgroundColor:"#013954",
-                    visualMap: { //图例值控制
-                        min: 0,
-                        max: 1,
-                        calculable: true,
-                        show: true,
-                        color: ['#f44336', '#fc9700', '#ffde00', '#ffde00', '#00eaff'],
-                        textStyle: {
-                            color: '#fff'
-                        }
-                    },
-                    geo: {
-                        map: 'world',
-                        zoom: 1.2,
-                        label: {
-                            emphasis: {
-                                show: false
-                            }
-                        },
-                        roam: true, //是否允许缩放
-                        itemStyle: {
-                            normal: {
-                                color: 'rgba(51, 69, 89, .5)', //地图背景色
-                                borderColor: '#516a89', //省市边界线00fcff 516a89
-                                borderWidth: 1
+                    baseOption: {
+                        timeline: {
+                            orient: 'vertical',
+                            top: '10',
+                            left: '90%',
+                            right: '20',
+                            bottom: '10',
+                            axisType: 'category',
+                            symbol: 'rect',
+                            symbolSize: [10, 6],
+                            itemStyle: {
+                                color: '#152a68',
+                            },
+                            checkpointStyle: {
+                                symbol: 'roundRect',
+                                color: '#3c8af1',
+                                symbolSize: [10, 6],
+                                borderWidth: 0,
+                            },
+                            controlStyle: {
+                                itemSize: 15,
                             },
                             emphasis: {
-                                color: 'rgba(37, 43, 61, .5)' //悬浮背景
+                                itemStyle: {
+                                    color: '#3c8af1',
+                                },
+                                label: {
+                                    color: '#3c8af1',
+                                },
+                                controlStyle: {
+                                    color: '#3c8af1',
+                                    borderColor: '3c8af1',
+                                    borderWidth: 2,
+                                },
+                            },
+                            label: {
+                                position: 10,
+                                interval: 1,
+                                color: '#aaa',
+                                fontSize: 10,
+                                formatter: value => {
+                                    const date = new Date(value)
+                                    return [date.getFullYear(),
+                                        date.getMonth() + 1,
+                                        date.getDate()]
+                                            .join('-')
+                                }
+                            },
+                            loop: true,
+                            autoPlay: true,
+                            playInterval: 4000,
+                            data: timelineData
+                        },
+                        tooltip: {
+                            trigger: 'item',
+                            backgroundColor: 'rgba(166, 200, 76, 0.82)',
+                            borderColor: '#FFFFCC',
+                            showDelay: 0,
+                            hideDelay: 0,
+                            enterable: true,
+                            transitionDuration: 0,
+                            extraCssText: 'z-index:100',
+                        },
+                        visualMap: { //图例值控制
+                            show: false,
+                            seriesIndex: 1,
+                            min: 0,
+                            max: 1,
+                            calculable: true,
+                            // color: ['#f44336', '#fc9700', '#ffde00', '#ffde00', '#00eaff'],
+                            textStyle: {
+                                color: '#fff'
                             }
-                        }
+                        },
+                        geo: {
+                            map: 'world',
+                            zoom: 1.2,
+                            label: {
+                                emphasis: {
+                                    show: false
+                                }
+                            },
+                            roam: true, //是否允许缩放
+                            itemStyle: {
+                                normal: {
+                                    color: 'rgba(51, 69, 89, .5)', //地图背景色
+                                    borderColor: '#516a89', //省市边界线00fcff 516a89
+                                    borderWidth: 1
+                                },
+                                emphasis: {
+                                    color: 'rgba(37, 43, 61, .5)' //悬浮背景
+                                }
+                            }
+                        },
+                        series: series
                     },
-                    series: series
+                    options,
                 };
                 const myChart = echarts.init(document.getElementById('trackMap'), 'light');
                 myChart.setOption(option);
             },
             initData() {
-                const trackArr = _.chain(TrackJSON)
+                const trackObj = _.chain(TrackJSON)
                     .filter(d => d.track.length > 0)
                     .map(d => {
                         const { track, blh } = d;
@@ -95,54 +143,84 @@
                             track: trackItem,
                         }
                     })
+                    .reduce((o, d) => {
+                        const key = d.track[0].time
+                        if (o[key]) {
+                            o[key].push(d)
+                        } else {
+                            o[key] = [d]
+                        }
+                        return o
+                    }, {})
+                    .pickBy('length')
                     .value()
-                return trackArr;
+                return trackObj;
+            },
+            initOptions() {
+                const timeTrack = this.initData()
+                const options = _.chain(timeTrack)
+                    .orderBy(d => new Date(d[0].track[0].time).getTime())
+                    .map(d => {
+                        const trackData = d.map(d1 => d1.track)
+                        const scatterData = _.chain(trackData)
+                            .flattenDeep()
+                            .reduce((obj, d) => {
+                                if(d.name === '深圳') return obj;
+                                let value = d.coord.concat(d.value);
+                                if(obj[d.name]) {
+                                    value[2] += 1;
+                                }
+                                obj[d.name] = {
+                                    name: d.name,
+                                    value,
+                                }
+                                return obj;
+                            }, {})
+                            .values()
+                            .value();
+                        return {
+                            series: [{
+                                data: trackData,
+                            },
+                            {
+                                data: scatterData,
+                            }]
+                        }
+                    })
+                    .value()
+
+                const timelineData = _.chain(timeTrack)
+                    .keys()
+                    .orderBy(d => new Date(d).getTime())
+                    .value()
+                return {options, timelineData}
             },
             initSeries() {
                 const series = [];
-                const trackData = this.initData();
-                const justTrack = trackData.map(d => d.track);
-                const startstationArr = _.chain(justTrack)
-                    .flattenDeep()
-                    .reduce((obj, d) => {
-                        if(d.name === '深圳') return obj;
-                        let value = d.coord.concat(d.value);
-                        if(obj[d.name]) {
-                            value[2] += 1;
-                        }
-                        obj[d.name] = {
-                            name: d.name,
-                            value,
-                        }
-                        return obj;
-                    }, {})
-                    .values()
-                    .value();
-
                 series.push(
                     {
-                        type: 'lines',
+                        type: "lines",
                         zlevel: 2,
                         effect: {
                             show: true,
-                            period: 4, //箭头指向速度，值越小速度越快
+                            period: 2, //箭头指向速度，值越小速度越快
                             trailLength: 0.02, //特效尾迹长度[0,1]值越大，尾迹越长重
-                            symbol: 'arrow', //箭头图标
+                            symbol: "arrow", //箭头图标
                             symbolSize: 5, //图标大小
+                            loop: false,
                         },
                         lineStyle: {
                             normal: {
                                 width: 1, //尾迹线条宽度
-                                opacity: 1, //尾迹线条透明度
-                                curveness: .3 //尾迹线条曲直度
+                                opacity: 0.05, //尾迹线条透明度
+                                curveness: 0.3 //尾迹线条曲直度
                             }
                         },
-                        data: justTrack,
                     },
                     {
                         type: 'effectScatter',
                         coordinateSystem: 'geo',
-                        zlevel: 2,
+                        zlevel: 1,
                         rippleEffect: { //涟漪特效
                             period: 4, //动画时间，值越小速度越快
                             brushType: 'stroke', //波纹绘制方式 stroke, fill
@@ -156,7 +234,7 @@
                                 formatter: function(params){//圆环显示文字
                                     return params.data.name;
                                 },
-                                fontSize: 13
+                                fontSize: 10
                             },
                             emphasis: {
                                 show: true
@@ -164,7 +242,7 @@
                         },
                         symbol: 'circle',
                         symbolSize: function(val) {
-                            return val[2] * 3 >= 6 ? 6 : val[2] * 3; //圆环大小
+                            return val[2] * 3 >= 6 ? 5 : val[2] * 3; //圆环大小
                         },
                         itemStyle: {
                             normal: {
@@ -172,7 +250,6 @@
                                 color: '#f00'
                             }
                         },
-                        data: startstationArr
                     },
                     {
                         type: 'scatter',
@@ -201,6 +278,7 @@
                         },
                         symbol: 'pin',
                         symbolSize: 20,
+                        z: 10,
                         data: [{
                             name: '深圳',
                             value: StationGeo['深圳'].concat([10]),
@@ -208,7 +286,7 @@
                     }
                 );
                 return series;
-            }
+            },
         },
         mounted() {
             this.initMap()
