062122e94f35c9c5f3f07f5f2ad272aee448b726
邓鑫波 <mymail33333@163.com>
2020-06-12 21:01:57 +0800

    更新

--- src/page/case/components/tree.vue
+++ src/page/case/components/tree.vue
@@ -1,16 +1,26 @@
 <template>
-    <div id="tree-chart">
+<div class="container">
+    <div id="tree-chart" :class="{'invisible': isHidden}">
 
+    </div>
+    <NoData title="请选择病例关系" :class="{'d-none': !isHidden}" />
     </div>
 </template>
 
 <script>
     import echarts from 'echarts/lib/echarts';
     import 'echarts/lib/chart/tree'
+    import NoData from '@/components/noData'
 
     export default {
         name: 'Tree',
         props: ['data'],
+        components: {NoData},
+        data() {
+            return {
+                isHidden: true
+            }
+        },
         methods: {
             initChart() {
                 var option = {
@@ -26,29 +36,45 @@
 
                             left: '2%',
                             right: '2%',
-                            top: '8%',
-                            bottom: '20%',
+                            // top: '8%',
+                            // bottom: '20%',
 
-                            symbol: 'emptyCircle',
+                            symbol: 'circle',
 
                             orient: 'vertical',
 
                             expandAndCollapse: true,
+                            // symbol: 'circle',
+                            symbolSize: 30,
+
+                            lineStyle: {
+                                curveness: 1,
+                                // color: 'rgba(255, 0, 0, .45)',
+                            },
+
+                            itemStyle: {
+                                color: 'rgba(255, 0, 0, .45)',
+                                borderWidth: 1,
+                                borderColor: 'rgba(255, 255, 255, 1)',
+                            },
 
                             label: {
-                                position: 'top',
-                                rotate: -90,
+                                position: 'inside',
+                                rotate: 0,
                                 verticalAlign: 'middle',
-                                align: 'right',
-                                fontSize: 9
+                                align: 'center',
+                                fontSize: 10,
+                                color: '#fff',
                             },
 
                             leaves: {
                                 label: {
                                     position: 'bottom',
-                                    rotate: -90,
+                                    rotate: 0,
                                     verticalAlign: 'middle',
-                                    align: 'left'
+                                    align: 'center',
+                                    color: '#fff',
+                                    distance: 10,
                                 }
                             },
 
@@ -64,19 +90,40 @@
         },
         watch: {
             data(newdata) {
+                this.isHidden = !newdata.children
+                this.myChart.resize()
                 const options = this.myChart.getOption();
                 options.series[0].data = [newdata];
                 this.myChart.setOption(options)
             }
         },
         mounted() {
+            console.log(this.data);
+            
             this.initChart();
         }
     }
 </script>
 
 <style lang="less" scoped>
+    .container {
+        height: 100%;
+        position: relative;
+    }
     #tree-chart{
         height: 400px;
+    }
+    .noData {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+    .invisible {
+        opacity: 0;
+    }
+    .d-none {
+        display: none;
     }
 </style>
